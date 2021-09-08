cc8ce93a164e89787576de3de2e83fa4e7d9ab46
Sonny Achid <35994735+sastro86@users.noreply.github.com>
2021-09-09 00:53:06 +0700

    Add files via upload

---
+++ application/views/bumitekno/css.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Welcome to CodeIgniter</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+
+
+    <style type="text/css">
+        body {
+            background: #ffff9b;
+        }
+
+        #container {
+            text-align: center;
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 10%;
+            width: 40%;
+        }
+
+        .rotate {
+            -webkit-transform: rotate(45deg);
+            -moz-transform: rotate(45deg);
+            -o-transform: rotate(45deg);
+            -ms-transform: rotate(45deg);
+            transform: rotate(45deg);
+        }
+
+        .box {
+            margin: auto;
+            height: 50px;
+            width: 50px;
+            display: inline-block;
+        }
+
+        .grn_1 {
+            background: #09a309;;
+        }
+
+        .grn_2 {
+            background: #72be20;
+        }
+
+        .text {
+            padding-top: 30px;
+            font-family: 'Tangerine', serif;
+            font-weight: bold;
+        }
+
+        .f22 {
+            font-size: 22px;
+        }
+
+        .f30 {
+            font-size: 30px;
+        }
+        .pt-0{
+            margin-bottom: 0px;
+        }
+        .pb-0{
+            padding-top: 0px;
+        }
+        .mb-0{
+            margin-bottom: 0px;
+        }
+        .mt-0{
+            margin-top: 0px;
+        }
+    </style>
+</head>
+<body>
+
+<div id="container">
+    <div class="rotate">
+        <div>
+            <a class="box grn_1"></a>
+            <a class="box grn_2"></a>
+            <a class="box grn_1"></a>
+        </div>
+        <div>
+            <a class="box grn_2"></a>
+            <a class="box grn_1"></a>
+            <a class="box grn_2"></a>
+        </div>
+        <div>
+            <a class="box grn_1"></a>
+            <a class="box grn_2"></a>
+            <a class="box grn_1"></a>
+        </div>
+    </div>
+
+    <div class="text">
+        <p class="f30 pb-0 mb-0">.: Selamat Hari Raya Idul Fitri :.</p>
+        <p class="f22 pt-0 mt-0">Mohon Maaf Lahir dan Batin</p>
+    </div>
+</div>
+
+
+</body>
+</html>
---
+++ application/views/bumitekno/pertama.php
@@ -0,0 +1,18 @@
+<div class="ttl">
+    Harga Bitcoin Hari Ini
+</div>
+
+<table class="tbl" id="infoBTC">
+    <thead>
+    <tr>
+        <th>Mata Uang</th>
+        <th>Harga Beli</th>
+        <th>Harga Jual</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td colspan="3">&nbsp;</td>
+    </tr>
+    </tbody>
+</table>
